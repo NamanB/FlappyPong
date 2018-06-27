3163ef6dea4d132520790dcc4e30c13a6617450a
nbhargava <namanb.edu@gmail.com>
2018-06-27 15:22:59 +0530

    added Ball attributes and created second initialization method

--- src/Ball.java
+++ src/Ball.java
@@ -3,8 +3,17 @@ import processing.core.PApplet;
 public class Ball {
 	PApplet game;
 	
+	int ballX = 0;
+	int ballY = 0;
+	int ballSize = 20;
+	int ballColor = game.color(0);
+	
 	public Ball(PApplet p) {
-		game = p;
+		this.game = p;
 	}
 	
+	public Ball(PApplet p, int color) {
+		this.game = p;
+		this.ballColor = game.color(color);
+	}
 }
