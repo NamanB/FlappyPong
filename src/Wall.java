12e6b71431d1c1c6fd7ded7d25298ec0465357c5
nbhargava <namanb.edu@gmail.com>
2018-07-07 11:28:04 +0530

    Added attributes to walls

--- src/Wall.java
+++ src/Wall.java
@@ -3,6 +3,11 @@ import processing.core.PApplet;
 public class Wall {
 	PApplet game;
 	
+	int minGapHeight = 200;
+	int maxGapHeight = 300;
+	int wallWidth = 80;
+	int wallColor = game.color(37, 104, 85);
+	
 	public Wall(PApplet p) {
 		game = p;
 	}
