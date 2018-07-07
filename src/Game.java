432f4a720166165cc1699d01a4d76483404b7be1
nbhargava <namanb.edu@gmail.com>
2018-07-07 11:16:30 +0530

    added Balls into Game and added setup

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 import processing.core.*;
 
 public class Game extends PApplet {
 	int activeScreen = 0; // 0 = Initial Screen, 1 = Game Screen, 2 = Game-Over Screen
+	ArrayList<Ball> balls = new ArrayList<Ball>();
+	
 	
 	public static void main(String[] args) {
 		PApplet.main("Game");
@@ -9,6 +13,13 @@ public class Game extends PApplet {
 	
 	public void settings() {
 		size(500, 500);
+	}
+	
+	public void setup() {
+		balls.add(new Ball(this, 20, 0));
+		Ball b = balls.get(0);
+		b.ballX = width / 4;
+		b.ballY = height / 5;
 	}
 	
 	public void draw() {
